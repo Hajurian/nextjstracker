692dd013d7bfaf383635e4f400045940ce803bb7
Hajurian <julianmoet1@gmail.com>
2024-06-13 17:38:30 -0400

    Worked more on layout of homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,40 @@
 import styles from "@/app/styles/home.module.css";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    return (
+      <>
+        <h1>No session</h1>
+      </>
+    );
+  }
+  const res = await fetch("http://localhost:3000/api/getUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: session?.user?.email,
+    }),
+  });
+  const user = await res.json();
   return (
     <>
       <div className={styles.homecontainer}>
         <div className={styles.topcontainer}>
+          <div className={styles.smallinfo}></div>
+          <div className={styles.smallinfo}></div>
+          <div className={styles.smallinfo}></div>
+        </div>
+        <div className={styles.middlecontainer}>
           <div className={styles.calendarcontainer}>Calendar</div>
           <div>Mr Incredibles thing</div>
         </div>
         <div className={styles.bottomcontainer}>
-          <div>Todo list</div>
-          <div>Habits</div>
+          <div className={styles.todobox}>Todo list</div>
+          <div className={styles.habitbox}>Habits</div>
         </div>
       </div>
     </>
